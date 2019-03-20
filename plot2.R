394450d3aa321fc1a13c56d86f74e98c19c0f89f
olarina <gar_o@bk.ru>
2019-03-20 14:03:09 +0100

    Update

--- plot2.R
+++ plot2.R
@@ -19,7 +19,6 @@ data$Date <- as.Date(data$Date,tryFormats = "%d/%m/%Y")
 
 ## Creates plot
 png(filename = "plot2.png", width = 480, height = 480)
-plot(data$Time, data$Global_active_power, type="n",
+plot(data$Time, data$Global_active_power, type="l",
      xlab="", ylab = "Global Active Power (kilowatts)")
-lines(data$Time, data$Global_active_power)
 dev.off()
